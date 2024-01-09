68dd87fc12b0c61388ebd8c5e88856cfcc4e43c7
Zahra <zahraahosnii@gmail.com>
2024-01-09 13:53:18 +0300

    Apo init

---
+++ android/app/src/main/kotlin/com/example/plant_disease_identification_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.plant_disease_identification_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,176 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_v2/tflite_v2.dart';
+
+// import 'package:tflite/tflite.dart';
+
+class HomeScreen extends StatefulWidget {
+  //static const String routeName = "Home";
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _loading = true;
+  late File _image;
+  late List _output;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  detectImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 8,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      _output = output!;
+      _loading = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/models/model_unquant.tflite",
+      labels: "assets/models/labels.txt",
+    );
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+
+  pickImage() async {
+    var image = await picker.pickImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _image = File(image.path);
+    });
+
+    detectImage(_image);
+  }
+
+  pickGalleryImage() async {
+    var image = await picker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _image = File(image.path);
+    });
+
+    detectImage(_image);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: _loading
+                  ? Container(
+                      width: 270,
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset("assets/images/Green Leaves.jpeg"),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                    )
+                  : Container(
+                      child: Column(
+                        children: <Widget>[
+                          //To display the image that the user picked
+
+                          Column(
+                            children: [
+                              Image.file(_image),
+                              Text("Error"),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "${_output[0]["label"]}",
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+            ),
+
+            //Buttons
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: pickImage,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 230,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 18),
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text("Camera",
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  GestureDetector(
+                    onTap: pickGalleryImage,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 230,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 18),
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text("Select a photo",
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
